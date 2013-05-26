d8661a7d643965f2eecb5590ad5ef4e032c824c3
Chris <shogo.thereal@gmail.com>
2013-05-26 19:25:40 +0200

    BaseActivity

---
+++ src/cat/uab/pfc/agp/helpdeskmanager/BaseActivity.java
@@ -0,0 +1,33 @@
+package cat.uab.pfc.agp.helpdeskmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public abstract class BaseActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		assignarViewsPerId();
+		configurarViews();
+	}
+
+	/**
+	 * Canviar les dades d'una vista. PEr exemple actualitzar els elements que
+	 * apareixen a una llista.
+	 */
+	protected abstract void actualitzarViews();
+
+	/**
+	 * Assignar les vistes d'un layout a vistes d'una activity fent servir
+	 * findViewById
+	 */
+	protected abstract void assignarViewsPerId();
+
+	/**
+	 * Afegir configuracio especial a les vistes. Per exemple assignar adapter a
+	 * un ListView
+	 */
+	protected abstract void configurarViews();
+
+}
